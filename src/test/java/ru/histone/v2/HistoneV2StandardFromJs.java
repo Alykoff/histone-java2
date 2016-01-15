b57e30c49ceb0689d6beec64cf10b3ccafe6be59
Alykoff.Gali <galll-al@yandex.kz>
2016-01-15 14:44:42 +0300

    add stardarthistonev2 for tests

---
+++ src/test/java/ru/histone/v2/HistoneV2StandardFromJs.java
@@ -0,0 +1,71 @@
+package ru.histone.v2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.lang.invoke.MethodHandles;
+import java.net.URL;
+import java.nio.file.Paths;
+
+/**
+ *
+ * Created by gali.alykoff on 14/01/16.
+ */
+public class HistoneV2StandardFromJs {
+    public static final String RESOURCE_PATH_TO_RUNNER = "resources/v2/tools/histone2_js_runner.js";
+    public static final String RELATIVE_RESOURCE_PATH_TO_RUNNER = "src/test/" + RESOURCE_PATH_TO_RUNNER;
+    private static final String PATH_TO_HISTONE2_JS_RUNNER = getRunnerJsAbsolutePath(RESOURCE_PATH_TO_RUNNER, RELATIVE_RESOURCE_PATH_TO_RUNNER);
+
+    public static void main(String[] args) throws IOException {
+        System.out.println(getNodes("2 x 2 == {{2 * 2}}"));
+        System.out.println(getTpl("2 x 2 == {{2 * 2}}"));
+    }
+
+    public static String getNodes(String tpl) throws IOException {
+        final ProcessBuilder processBuilder = new ProcessBuilder(
+                "node", PATH_TO_HISTONE2_JS_RUNNER, "--tpl='" + tpl + "'", "-n"
+        );
+        return getProcessResult(processBuilder);
+    }
+
+    public static String getTpl(String tpl) throws IOException {
+        final ProcessBuilder processBuilder = new ProcessBuilder(
+                "node", PATH_TO_HISTONE2_JS_RUNNER, "--tpl='" + tpl + "'"
+        );
+        return getProcessResult(processBuilder);
+    }
+
+    private static String getProcessResult(ProcessBuilder builder) throws IOException {
+        final Process process = builder.start();
+        try (
+                final InputStream is = process.getInputStream();
+                final InputStreamReader inputStreamReader = new InputStreamReader(is);
+                final BufferedReader bufferedReader = new BufferedReader(inputStreamReader)
+        ) {
+            String line;
+            String processResult = "";
+            while ((line = bufferedReader.readLine()) != null) {
+                processResult += line;
+            }
+            return processResult;
+        }
+    }
+
+    private static String getRunnerJsAbsolutePath(String resourcePath, String relativePath) {
+        final URL uriScript = MethodHandles
+                .lookup()
+                .lookupClass()
+                .getClassLoader()
+                .getResource(resourcePath);
+        try {
+            if (uriScript == null) {
+                return Paths.get(RELATIVE_RESOURCE_PATH_TO_RUNNER).toAbsolutePath().toString();
+            } else {
+                return Paths.get(uriScript.toURI()).toAbsolutePath().toString();
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("histone2 js runner script not found", e);
+        }
+    }
+}
---
+++ src/test/resources/v2/tools/histone2_js_runner.js
@@ -0,0 +1,68 @@
+'use strict';
+const Histone = require('histone');
+const FS = require('fs');
+const DEFAULT_SETTINGS = {
+    expression: '',
+    isReturnNodes: false,
+    isHelp: true
+};
+const FILE_ENCODING = 'utf8';
+
+var settings = getSettings();
+var expression = settings.expression;
+var template = Histone(expression);
+var info = 'Options:\n' +
+    '\t--file=index.tpl\tHistone template in file\n' +
+    '\t--tpl=\'{{2 * 2}}\'\tHistone template\n' +
+    '\t-returnNode, -n\t\tReturn Nodes\n' +
+    '\t-help, -about\t\tHelp info\n';
+
+if (settings.isHelp) {
+    console.log(info);
+}
+
+if (settings.isReturnNodes) {
+    console.log(template.b);
+} else {
+    template.render(console.log);
+}
+
+function getSettings() {
+    const TPL_ARG_FLAG = '--tpl=';
+    const FILE_ARG_FLAG = '--file=';
+    const RETURN_NODE_ARG_FLAG = '-returnNode';
+    const RETURN_NODE_SHORT_ARG_FLAG = '-n';
+    const HELP_ARG_FLAG = '-help';
+    const ABOUT_ARG_FLAG = '-about';
+
+    var settings = Object.assign({}, DEFAULT_SETTINGS);
+    var usingHelpFlag = false;
+    for (var key in process.argv) {
+        var value = process.argv[key];
+        if (value.startsWith(TPL_ARG_FLAG)) {
+            var tplContent = value.substring(TPL_ARG_FLAG.length, value.length);
+            var isQuotes = tplContent.startsWith("'") && tplContent.endsWith("'");
+            if (isQuotes) {
+                tplContent = tplContent.substring(1, tplContent.length);
+                tplContent = tplContent.substring(0, tplContent.length - 1);
+            }
+            settings.expression = tplContent;
+            settings.isHelp = false;
+        } else if (value.startsWith(FILE_ARG_FLAG)) {
+            var path = value.substring(FILE_ARG_FLAG.length, value.length);
+            settings.expression = FS.readFileSync(path, FILE_ENCODING, (err, data) => {
+                if (err) {
+                    throw Error(err);
+                }
+            }).toString();
+            settings.isHelp = false;
+        } else if (value === RETURN_NODE_ARG_FLAG || value === RETURN_NODE_SHORT_ARG_FLAG) {
+            settings.isReturnNodes = true;
+            settings.isHelp = false;
+        } else if (value === HELP_ARG_FLAG || value === ABOUT_ARG_FLAG) {
+            usingHelpFlag = true;
+        }
+    };
+    settings.isHelp = settings.isHelp || usingHelpFlag;
+    return settings;
+}

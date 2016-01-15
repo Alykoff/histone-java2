8d843c8701f81e96dc222e7c5d8c18e15be3152a
Alykoff.Gali <galll-al@yandex.kz>
2016-01-15 19:32:47 +0300

    split test cases

--- src/test/java/ru/histone/v2/ParserTest.java
+++ src/test/java/ru/histone/v2/ParserTest.java
@@ -1,38 +1,58 @@
 package ru.histone.v2;
 
+import junit.framework.Assert;
 import org.junit.Test;
+import org.junit.runner.Description;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.RunWith;
+import org.junit.runner.Runner;
+import org.junit.runner.notification.RunNotifier;
+import org.junit.runners.Parameterized;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ru.histone.HistoneException;
 import ru.histone.v2.test.TestRunner;
 import ru.histone.v2.test.dto.HistoneTestCase;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 /**
+ *
  * Created by alexey.nevinsky on 25.12.2015.
  */
+@RunWith(Parameterized.class)
 public class ParserTest extends BaseTest {
-
     private static final Logger LOG = LoggerFactory.getLogger(ParserTest.class);
+    public static final String BASE_IF_STATEMENT_JSON = "base/ifStatement.json";
 
-    private void runTest(String filePath) throws HistoneException {
-        List<HistoneTestCase> cases = TestRunner.loadTestCase(filePath);
-        for (HistoneTestCase histoneTestCase : cases) {
+    private String input;
+    private String expected;
+
+    public ParserTest(String input, String expected) {
+        this.input = input;
+        this.expected = expected;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        final List<Object[]> result = new ArrayList<>();
+        final List<HistoneTestCase> histoneTestCases = TestRunner.loadTestCase(BASE_IF_STATEMENT_JSON);
+        for (HistoneTestCase histoneTestCase : histoneTestCases) {
             System.out.println("Run test '" + histoneTestCase.getName() + "'");
             for (HistoneTestCase.Case testCase : histoneTestCase.getCases()) {
                 System.out.println("Expression: " + testCase.getInput());
-                try {
-                    doTest(testCase.getInput(), testCase.getExpectedResult());
-                } catch (Throwable ignore) {
-                    System.out.println("down");
-                }
+                result.add(new Object[] {
+                        testCase.getInput(), testCase.getExpectedResult()
+                });
             }
         }
+        return result;
     }
 
     @Test
-    public void ifTest() throws HistoneException {
-        runTest("base/ifStatement.json");
+    public void test() throws HistoneException {
+        doTest(input, expected);
     }
 }

1b11ec40d88b347d5d9bebaf5ccd7199440ab960
inv3r <anevinsky@gmail.com>
2016-01-28 19:40:32 +0300

    update copyrights

--- src/main/java/ru/histone/v2/parser/tokenizer/Tokenizer.java
+++ src/main/java/ru/histone/v2/parser/tokenizer/Tokenizer.java
@@ -1,10 +1,24 @@
+/*
+ * Copyright (c) 2016 MegaFon
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package ru.histone.v2.parser.tokenizer;
 
 import org.apache.commons.lang.ArrayUtils;
 import ru.histone.expression.Expression;
 import ru.histone.tokenizer.Token;
-import static ru.histone.tokenizer.Tokens.T_ERROR;
-import static ru.histone.tokenizer.Tokens.T_EOF;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
@@ -13,6 +27,9 @@ import java.util.Queue;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+
+import static ru.histone.tokenizer.Tokens.T_EOF;
+import static ru.histone.tokenizer.Tokens.T_ERROR;
 
 /**
  * Created by alexey.nevinsky on 21.12.2015.

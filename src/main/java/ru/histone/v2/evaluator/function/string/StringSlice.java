63160707c29d2fcdda671fead565ae28a18b5741
Alykoff.Gali <galll-al@yandex.kz>
2016-02-19 19:16:35 +0300

    - done slice for string

--- src/main/java/ru/histone/v2/evaluator/function/string/StringSlice.java
+++ src/main/java/ru/histone/v2/evaluator/function/string/StringSlice.java
@@ -1,11 +1,15 @@
 package ru.histone.v2.evaluator.function.string;
 
 import ru.histone.v2.evaluator.Context;
+import ru.histone.v2.evaluator.EvalUtils;
 import ru.histone.v2.evaluator.function.AbstractFunction;
+import ru.histone.v2.evaluator.node.EmptyEvalNode;
 import ru.histone.v2.evaluator.node.EvalNode;
+import ru.histone.v2.evaluator.node.StringEvalNode;
 import ru.histone.v2.exceptions.FunctionExecutionException;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 
 /**
@@ -13,6 +17,8 @@ import java.util.concurrent.CompletableFuture;
  */
 public class StringSlice extends AbstractFunction {
     public static final String NAME = "slice";
+    public static final int DEFALUT_START = 0;
+    public static final CompletableFuture<EvalNode> EMPTY_RESULT = CompletableFuture.completedFuture(EvalUtils.createEvalNode(""));
 
     @Override
     public String getName() {
@@ -21,6 +27,45 @@ public class StringSlice extends AbstractFunction {
 
     @Override
     public CompletableFuture<EvalNode> execute(Context context, List<EvalNode> args) throws FunctionExecutionException {
-        return null;
+        final String value = ((StringEvalNode) args.get(0)).getValue();
+        final int strLen = value.length();
+        final Optional<Integer> startRaw = args.size() > 1
+                ? EvalUtils.tryPureIntegerValue(args.get(1))
+                : Optional.of(DEFALUT_START);
+        final Optional<Integer> lengthRaw = args.size() > 2
+                ? EvalUtils.tryPureIntegerValue(args.get(2))
+                : Optional.of(strLen);
+        if (!startRaw.isPresent() || !lengthRaw.isPresent()) {
+            return CompletableFuture.completedFuture(EmptyEvalNode.INSTANCE);
+        }
+        int start = startRaw.get();
+        int length = lengthRaw.get();
+
+        if (start < 0) {
+            start = strLen + start;
+        }
+        if (start < 0) {
+            start = 0;
+        }
+        if (start >= strLen) {
+            return EMPTY_RESULT;
+        }
+
+        if (length == 0) {
+            length = strLen - start;
+        }
+        if (length < 0) {
+            length = strLen - start + length;
+        }
+        if (length <= 0) {
+            return EMPTY_RESULT;
+        }
+        if (length > strLen) {
+            length = strLen;
+        }
+
+        return CompletableFuture.completedFuture(
+                EvalUtils.createEvalNode(value.substring(start, length))
+        );
     }
 }

47b011111433b295ab629b203991b1b423f80c05
Alykoff.Gali <galll-al@yandex.kz>
2016-02-19 18:29:25 +0300

    - done split func for string

--- src/main/java/ru/histone/v2/evaluator/function/string/StringSplit.java
+++ src/main/java/ru/histone/v2/evaluator/function/string/StringSplit.java
@@ -1,10 +1,13 @@
 package ru.histone.v2.evaluator.function.string;
 
 import ru.histone.v2.evaluator.Context;
+import ru.histone.v2.evaluator.EvalUtils;
 import ru.histone.v2.evaluator.function.AbstractFunction;
 import ru.histone.v2.evaluator.node.EvalNode;
+import ru.histone.v2.evaluator.node.StringEvalNode;
 import ru.histone.v2.exceptions.FunctionExecutionException;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
@@ -13,6 +16,7 @@ import java.util.concurrent.CompletableFuture;
  */
 public class StringSplit extends AbstractFunction {
     public static final String NAME = "split";
+    public static final String DEFAULT_SEPARATOR = "";
 
     @Override
     public String getName() {
@@ -21,6 +25,17 @@ public class StringSplit extends AbstractFunction {
 
     @Override
     public CompletableFuture<EvalNode> execute(Context context, List<EvalNode> args) throws FunctionExecutionException {
-        return null;
+        final String value = ((StringEvalNode) args.get(0)).getValue();
+        final String separator;
+        if (args.size() > 1 && EvalUtils.isStringNode(args.get(1))) {
+            separator = ((StringEvalNode) args.get(1)).getValue();
+        } else {
+            separator = DEFAULT_SEPARATOR;
+        }
+        return CompletableFuture.completedFuture(
+                EvalUtils.constructFromList(
+                        Arrays.asList(value.split(separator))
+                )
+        );
     }
 }

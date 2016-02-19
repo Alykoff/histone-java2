07afffe367be70a54102528110f7fb1d0883cada
Alykoff.Gali <galll-al@yandex.kz>
2016-02-19 17:51:17 +0300

    - done charCodeAt for string

--- src/main/java/ru/histone/v2/evaluator/function/string/StringCharCodeAt.java
+++ src/main/java/ru/histone/v2/evaluator/function/string/StringCharCodeAt.java
@@ -1,11 +1,13 @@
 package ru.histone.v2.evaluator.function.string;
 
 import ru.histone.v2.evaluator.Context;
+import ru.histone.v2.evaluator.EvalUtils;
 import ru.histone.v2.evaluator.function.AbstractFunction;
-import ru.histone.v2.evaluator.node.EvalNode;
+import ru.histone.v2.evaluator.node.*;
 import ru.histone.v2.exceptions.FunctionExecutionException;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 
 /**
@@ -21,6 +23,18 @@ public class StringCharCodeAt extends AbstractFunction {
 
     @Override
     public CompletableFuture<EvalNode> execute(Context context, List<EvalNode> args) throws FunctionExecutionException {
-        return null;
+        final Optional<Integer> indexOptional = EvalUtils.tryPureIntegerValue(args.get(1));
+        if (!indexOptional.isPresent()) {
+            return CompletableFuture.completedFuture(EmptyEvalNode.INSTANCE);
+        }
+        final String value = ((StringEvalNode) args.get(0)).getValue();
+        int index = indexOptional.get();
+        int lenght = value.length();
+        if (index < 0) index = lenght + index;
+        if (index >= 0 && index < lenght) {
+            final int character = (int) value.charAt(index);
+            return CompletableFuture.completedFuture(new LongEvalNode(character));
+        }
+        return CompletableFuture.completedFuture(EmptyEvalNode.INSTANCE);
     }
 }

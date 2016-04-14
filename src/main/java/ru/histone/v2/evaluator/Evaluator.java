3865e36ac210cf054074eb3adc15664230fc6f3e
Alexey Nevinsky <alexey.nevinsky@billing.ru>
2016-04-14 16:03:03 +0300

    fixed long executing of getting regexp value

--- src/main/java/ru/histone/v2/evaluator/Evaluator.java
+++ src/main/java/ru/histone/v2/evaluator/Evaluator.java
@@ -949,29 +949,30 @@ public class Evaluator implements Serializable {
     }
 
     private CompletableFuture<EvalNode> processRegExp(ExpAstNode node) {
-        return CompletableFuture.supplyAsync(() -> {
-            final LongAstNode flagsNumNode = node.getNode(1);
-            final long flagsNum = flagsNumNode.getValue();
+        return CompletableFuture.completedFuture(null)
+                .thenApply(nullValue -> {
+                    final LongAstNode flagsNumNode = node.getNode(1);
+                    final long flagsNum = flagsNumNode.getValue();
 
-            boolean isIgnoreCase = false;
-            boolean isMultiline = false;
+                    boolean isIgnoreCase = false;
+                    boolean isMultiline = false;
 
-            int flags = 0;
-            if ((flagsNum & AstRegexType.RE_IGNORECASE.getId()) != 0) {
-                flags |= Pattern.CASE_INSENSITIVE;
-                isIgnoreCase = true;
-            }
-            if ((flagsNum & AstRegexType.RE_MULTILINE.getId()) != 0) {
-                flags |= Pattern.MULTILINE;
-                isMultiline = true;
-            }
+                    int flags = 0;
+                    if ((flagsNum & AstRegexType.RE_IGNORECASE.getId()) != 0) {
+                        flags |= Pattern.CASE_INSENSITIVE;
+                        isIgnoreCase = true;
+                    }
+                    if ((flagsNum & AstRegexType.RE_MULTILINE.getId()) != 0) {
+                        flags |= Pattern.MULTILINE;
+                        isMultiline = true;
+                    }
 
-            final boolean isGlobal = (flagsNum & AstRegexType.RE_GLOBAL.getId()) != 0;
-            final StringAstNode expNode = node.getNode(0);
-            final String exp = expNode.getValue();
-            final Pattern pattern = Pattern.compile(exp, flags);
-            return new RegexEvalNode(new HistoneRegex(isGlobal, isIgnoreCase, isMultiline, pattern));
-        });
+                    final boolean isGlobal = (flagsNum & AstRegexType.RE_GLOBAL.getId()) != 0;
+                    final StringAstNode expNode = node.getNode(0);
+                    final String exp = expNode.getValue();
+                    final Pattern pattern = Pattern.compile(exp, flags);
+                    return new RegexEvalNode(new HistoneRegex(isGlobal, isIgnoreCase, isMultiline, pattern));
+                });
     }
 
 }

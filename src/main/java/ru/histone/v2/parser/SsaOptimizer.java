4096a072d833dcec91084408997d7a270a66b196
Alexey Nevinsky <alexey.nevinsky@billing.ru>
2016-06-22 18:15:29 +0300

    fixed optimizer. Added tests

--- src/main/java/ru/histone/v2/parser/SsaOptimizer.java
+++ src/main/java/ru/histone/v2/parser/SsaOptimizer.java
@@ -16,6 +16,7 @@
 
 package ru.histone.v2.parser;
 
+import ru.histone.v2.Constants;
 import ru.histone.v2.parser.node.*;
 import ru.histone.v2.utils.Tuple;
 
@@ -27,7 +28,6 @@ import java.util.*;
 public class SsaOptimizer {
 
     private Deque<Scope> scopes = new ArrayDeque<>();
-    private boolean isMacroScope = false;
 
     public void process(AstNode root) {
         enter();
@@ -55,16 +55,92 @@ public class SsaOptimizer {
                 }
             }
             break;
+            case AST_NODES: {
+                enter();
+                processNodes(currentNode.getNodes());
+                leave();
+            }
+            break;
+            case AST_WHILE: {
+                if (currentNode.size() > 1) {
+                    processNode(currentNode.getNode(1));
+                }
+
+                enter();
+                getVarName(Constants.SELF_NAME);
+                processNode(currentNode.getNode(0)); //bcz we don't need to process condition node in current scope
+                leave();
+            }
+            break;
             case AST_REF: {
                 ValueNode n = currentNode.getNode(1);
+                LongAstNode scopeDiff = currentNode.getNode(0);
                 if (n instanceof StringAstNode) {
-                    Tuple<Long, Long> refId = getRefPair(((StringAstNode) currentNode.getNode(1)).getValue());
+                    Tuple<Long, Long> refId = getRefPair(scopeDiff.getValue(), ((StringAstNode) currentNode.getNode(1)).getValue());
                     currentNode.setNode(1, new LongAstNode(refId.getRight()));
                 } else {
-                    Tuple<Long, Long> refId = getRefPair(n.getValue() + "");
+                    Tuple<Long, Long> refId = getRefPair(scopeDiff.getValue(), n.getValue() + "");
                     if (refId != null) {
                         currentNode.setNode(1, new LongAstNode(refId.getRight()));
                     }
+                }
+            }
+            break;
+            case AST_FOR: {
+                enter();
+                getVarName(Constants.SELF_NAME);
+                if (((ValueNode) currentNode.getNode(0)).getValue() != null) {
+                    getVarName(((ValueNode) currentNode.getNode(0)).getValue() + "");
+                }
+                if (((ValueNode) currentNode.getNode(1)).getValue() != null) {
+                    getVarName(((ValueNode) currentNode.getNode(1)).getValue() + "");
+                }
+                processNode(currentNode.getNode(2));
+                leave();
+
+                int i = 3;
+
+                AstNode n;
+                while ((n = currentNode.getNode(i)) != null) {
+                    if (i % 2 == 0) {
+                        enter();
+                        processNode(n);
+                        leave();
+                    } else {
+                        processNode(n);
+                    }
+                    i++;
+                }
+            }
+            break;
+            case AST_MACRO: {
+                enter();
+                getVarName(Constants.SELF_NAME);
+                if (currentNode.size() > 2) {
+                    LongAstNode argsSize = currentNode.getNode(2);
+                    for (long i = 0; i < argsSize.getValue(); i++) {
+                        getVarName((i + 1) + "");
+                    }
+                }
+
+                processNode(currentNode.getNode(1));
+                leave();
+            }
+            break;
+            case AST_IF: {
+                int i = 0;
+
+                AstNode n;
+                while ((n = currentNode.getNode(i)) != null) {
+                    if (i % 2 == 0) {
+                        enter();
+                        processNode(n);
+                        leave();
+                    } else {
+                        processNode(n);
+                        int k = 0;
+                    }
+                    i++;
                 }
             }
             break;
@@ -80,16 +156,16 @@ public class SsaOptimizer {
         }
     }
 
-    public void enter() {
+    private void enter() {
         scopes.addFirst(new Scope());
     }
 
-    public void leave() {
+    private void leave() {
         scopes.pop();
     }
 
 
-    public Long getVarName(String varName) {
+    private Long getVarName(String varName) {
         Scope scope = scopes.peek();
 
         Var var = scope.vars.get(varName);
@@ -104,10 +180,17 @@ public class SsaOptimizer {
         return var.lastName();
     }
 
-    public Tuple<Long, Long> getRefPair(final String name) {
+    private Tuple<Long, Long> getRefPair(long scopeDiff, final String name) {
         Iterator<Scope> iterator = scopes.iterator();
         long i = 0;
+        long diff = 0;
         while (iterator.hasNext()) {
+            //we search scope by diff
+            if (diff != scopeDiff) {
+                diff++;
+                iterator.next();
+                continue;
+            }
             Scope scope = iterator.next();
             Var var = scope.vars.get(name);
             if (var != null) {
@@ -119,12 +202,12 @@ public class SsaOptimizer {
         return null;
     }
 
-    public static class Scope {
+    private static class Scope {
         private long counter = 0;
         private Map<String, Var> vars = new HashMap<>();
     }
 
-    public static class Var {
+    private static class Var {
         private boolean used = false;
         private List<Long> names = new ArrayList<>();
 
